844fcebbfa64c2e9e4cdc10cd1d279f36a7d1bda
Emilia van der Laan <e.vanderlaan@student.fontys.nl>
2024-09-09 10:45:36 +0200

    Slagboom V3

--- main.go
+++ main.go
@@ -7,27 +7,54 @@ import (
 	"time"
 )
 
-//Prints text to the console.
+//Making a hardcoded string of license plates for testing purposes
+
+var kentekens = []string{
+
+	"ken-tek-en", "tes-ten", "344-321",
+}
+
+// functie om te controleren of kenteken correct is
+func scanteken(kents string) bool {
+
+	for _, k := range kentekens {
+
+		if k == kents {
+
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 
-	tijdophalen := time.Now().Hour()
+	var kenteken string
+	fmt.Print("Voer uw kenteken in: ")
+	fmt.Scanln(&kenteken)
 
-	fmt.Print("Welcome to Fonteyn, Het beste vakantiepark volgens de consumentenbond\n")
+	if scanteken(kenteken) {
 
-	switch {
+		tijdophalen := time.Now().Hour()
 
-	case tijdophalen > 7 && tijdophalen < 12:
-		fmt.Print("Goedemorgen")
+		fmt.Print("Welcome to Fonteyn, Het beste vakantiepark volgens de consumentenbond\n")
 
-	case tijdophalen >= 12 && tijdophalen <= 18:
-		fmt.Print("Goedemiddag")
+		switch {
 
-	case tijdophalen >= 18 && tijdophalen <= 23:
-		fmt.Print("goedeavond")
+		case tijdophalen > 7 && tijdophalen < 12:
+			fmt.Print("Goedemorgen")
 
-	default:
-		fmt.Print("We zijn gesloten")
+		case tijdophalen >= 12 && tijdophalen <= 18:
+			fmt.Print("Goedemiddag")
 
+		case tijdophalen >= 18 && tijdophalen <= 23:
+			fmt.Print("goedeavond")
+
+		default:
+			fmt.Print("We zijn gesloten")
+
+		}
+	} else {
+		fmt.Print("U heeft helaas geen toegang tot het parkeerterrein")
 	}
 }
