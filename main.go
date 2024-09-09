36f6300115c1a29ca5c2b03eceaeae676f52d757
Emilia van der Laan <e.vanderlaan@student.fontys.nl>
2024-09-09 10:07:56 +0200

    Slagboom V2

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 //imports the function FMT
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 //Prints text to the console.
 
 func main() {
 
-	fmt.Print("Welcome to Fonteyn, Het beste vakantiepark volgens de consumentenbond")
+	tijdophalen := time.Now().Hour()
 
+	fmt.Print("Welcome to Fonteyn, Het beste vakantiepark volgens de consumentenbond\n")
+
+	switch {
+
+	case tijdophalen > 7 && tijdophalen < 12:
+		fmt.Print("Goedemorgen")
+
+	case tijdophalen >= 12 && tijdophalen <= 18:
+		fmt.Print("Goedemiddag")
+
+	case tijdophalen >= 18 && tijdophalen <= 23:
+		fmt.Print("goedeavond")
+
+	default:
+		fmt.Print("We zijn gesloten")
+
+	}
 }
